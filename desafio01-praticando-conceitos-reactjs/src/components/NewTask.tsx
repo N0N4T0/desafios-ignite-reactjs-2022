1764526e3204fc2857162ea6b1336092da314188
LUCIO Notebook <luciononato@hotmail.com>
2022-06-12 09:26:03 -0300

    feat(components)add-taskComponents

--- desafio01-praticando-conceitos-reactjs/src/components/NewTask.tsx
+++ desafio01-praticando-conceitos-reactjs/src/components/NewTask.tsx
@@ -1,18 +1,30 @@
+import { PlusCircle } from 'phosphor-react'
 import styles from './NewTask.module.css'
 
 export function NewTask(){
     return(
         <main className={styles.main}>
             <form>
-                <input type="text" name="" />
+                <input type="text" name="" placeholder='Adicione uma tarefa'/>
 
                 <footer>
                     <button type="submit">
                         Criar
+                        <PlusCircle size={16} />
                     </button>
                 </footer>
             </form>
         
+            <div className={styles.tasksContainer}>
+                <div className={styles.newTask}>
+                    Tarefas criadas
+                    <span>0</span>
+                </div>
+                <div className={styles.taskDone}>
+                    Concluídas
+                    <span>0</span>
+                </div>
+            </div>
         </main>
     )
 }
