e1936dea4631245b8b7e66ee0d12365f763da88c
Lucio <luciononato@hotmail.com>
2023-02-26 12:41:21 -0300

    fix: change input to button

--- desafio01-praticando-conceitos-reactjs/src/components/Tasks/components/Task/index.tsx
+++ desafio01-praticando-conceitos-reactjs/src/components/Tasks/components/Task/index.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle, Trash } from "phosphor-react";
+import { Check, Trash } from "phosphor-react";
 import styles from "./Task.module.css";
 import { TaskProps } from "./types";
 
@@ -7,12 +7,27 @@ export function Task(props: TaskProps) {
 
   const { id, isCompleted, text } = task;
 
+  function verifyCompletedTask() {
+    if (task.isCompleted) {
+      return (
+        <div className={styles.completedSelectedTask}>
+          <Check className={styles.checkIconClass} size={12} />
+        </div>
+      );
+    }
+
+    return <div className={styles.uncompletedTask} />;
+  }
+
   return (
     <li className={styles.cardInformation}>
-      {/* //ToDO add button */}
-      <input type="checkbox" className={styles.checkbox} />
-      <p>{text}</p>
-      <button className={styles.trash}>
+      <button className={styles.selectedTask} onClick={() => onComplete(id)}>
+        {verifyCompletedTask()}
+      </button>
+
+      <p className={isCompleted ? styles.textTaskIsCompleted : ""}>{text}</p>
+
+      <button className={styles.trash} onClick={() => onDelete(id)}>
         <Trash size={24} />
       </button>
     </li>
